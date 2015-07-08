2891d8ae7ecf32d69e2e5d7bb94fe94483b81b8e
Christian Hansen <c.gamer21000@gmail.com>
2015-07-07 20:27:04 -0400

    save

--- utils.py
+++ utils.py
@@ -2,12 +2,12 @@
 
 import colors as c
 
-def prompt(question):
+def ask(question):
     print(c.red + question + c.reset)
     answer = input("> " + c.base3).lower().strip()
     print(c.reset)
     return answer
 
 if __name__ == '__main__':
-    testanswer = prompt("Whatever")
+    testanswer = ask("Whatever")
     print(testanswer)
